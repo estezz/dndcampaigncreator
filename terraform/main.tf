16862311928df2abb6e34615f41e7583230e291d
estezz <estezz@gmail.com>
2025-12-30 21:57:27 +0000

    dnd

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,13 +161,13 @@ resource "aws_lb_listener" "listener" {
 resource "aws_ecs_service" "app_service" {
   name            = "app-first-service" 
   cluster         = "${aws_ecs_cluster.dnd_cluster.id}"   # Reference the created Cluster
-  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
+  task_definition = "${aws_ecs_task_definition.dnd_task.arn}" # Reference the task that the service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Set up the number of containers to 3
 
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
-    container_name   = "${aws_ecs_task_definition.app_task.family}"
+    container_name   = "${aws_ecs_task_definition.dnd_task.family}"
     container_port   = 5000 # Specify the container port
   }
 
